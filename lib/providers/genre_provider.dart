7c1b2ff91a59cf3f9d5fb2029c6b962ab77b4ae3
Emmanuele <emagnubcp@gmail.com>
2024-02-23 21:02:46 +0000

    added genre

---
+++ lib/providers/genre_provider.dart
@@ -0,0 +1,11 @@
+//   ///
+//  Import LIBRARIES ILES
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+//  Import FILES
+//  PARTS
+part 'genre_provider.g.dart';
+//  PROVIDERS
+//  //  //   ///
+
+@riverpod
+String genre(GenreRef ref) => 'Shooter';
---
+++ lib/providers/genre_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'genre_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$genreHash() => r'f588065826752264353c501789871ad323dfac53';
+
+/// See also [genre].
+@ProviderFor(genre)
+final genreProvider = AutoDisposeProvider<String>.internal(
+  genre,
+  name: r'genreProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$genreHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GenreRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
